858ef14852ff535bed16843c793017391efaae4a
K1-R1 <kyranrawlinson@gmail.com>
2022-04-09 17:31:44 +0100

    Updated DCA.sol

--- contracts/DCA.sol
+++ contracts/DCA.sol
@@ -19,8 +19,47 @@ contract UNIDCA {
     IREWARD public rewardToken;
     IUNIOracle public priceOracle;
 
+    struct userAccount {
+        uint256 ethBalance;
+        uint256 ethValuePerWeek;
+        uint256 rewardBalance;
+        uint256 totalWeeks;
+        uint256 currentWeek;
+        uint256 lastSwapTimestamp;
+    }
+
+    mapping(address => userAccount) public addressToUserAccount; //only one account
+
+    uint256 public userCounter;
+
     constructor(address _rewardToken, address _priceOracle) {
         rewardToken = IREWARD(_rewardToken);
         priceOracle = IUNIOracle(_priceOracle);
+    }
+
+    function beginDCA(uint256 _weeks) external payable {
+        //reentrancy?
+        /**
+       
+        makes first swap
+        */
+        require(
+            (msg.value / _weeks) >= 0.01 ether,
+            "UNIDCA error: Minimum swap value in ether is 0.01, reduce number of weeks or increase ETH deposit"
+        );
+        require(
+            _weeks >= 2,
+            "UNIDCA error: number of weeks must be at least 2"
+        );
+        addressToUserAccount[msg.sender] = userAccount({
+            ethBalance: msg.value,
+            ethValuePerWeek: msg.value / _weeks,
+            rewardBalance: 0,
+            totalWeeks: _weeks,
+            currentWeek: 0,
+            lastSwapTimestamp: 0
+        });
+
+        swap();
     }
 }
