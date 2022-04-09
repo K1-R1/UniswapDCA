1c03b964bfe41eeb74a2c2aa527876bb0291b396
K1-R1 <kyranrawlinson@gmail.com>
2022-04-09 16:41:00 +0100

    Updated REWARD.sol

--- contracts/REWARD.sol
+++ contracts/REWARD.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract REWARD is ERC20 {
+contract REWARD is ERC20, Ownable {
     constructor() ERC20("REWARD", "RWD") {
         _mint(msg.sender, 10000000 * 10**decimals());
+    }
+
+    function setupNewOwner(address _newOwner) external onlyOwner {
+        transfer(_newOwner, balanceOf(owner()));
+        transferOwnership(_newOwner);
     }
 }
