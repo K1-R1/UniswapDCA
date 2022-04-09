f2075cd4b3174cf2b52d85a04564fa7b84321a9d
K1-R1 <kyranrawlinson@gmail.com>
2022-04-09 16:46:19 +0100

    Updated DCA.sol; with interfaces

--- contracts/DCA.sol
+++ contracts/DCA.sol
@@ -1,3 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
+
+interface IREWARD {
+    function transfer(address to, uint256 amount) external returns (bool);
+}
+
+interface IUNIOracle {
+    function update() external;
+
+    function consult(address token, uint256 amountIn)
+        external
+        view
+        returns (uint256 amountOut);
+}
+
+contract
